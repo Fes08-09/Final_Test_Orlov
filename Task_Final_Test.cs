932bb9cd144dcc4f3a1106d8a650cda95d1f4433
Dmitriy <Fes08-09@yandex.ru>
2022-08-06 16:48:39 +0300

    The programm is writen

---
+++ Task_Final_Test.cs
@@ -0,0 +1,62 @@
+﻿// Задача: написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше либо равна 3 символам. 
+// Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма.
+
+
+using System;
+using static System.Console;
+
+Clear();
+
+WriteLine("Введите количество слов в массиве: ");
+int count = int.Parse(ReadLine());
+if (count <= 0)
+{
+    WriteLine("Error. The value of count is incorrect.");
+    return;
+}
+WriteLine("Введите содержание массива (слова): ");
+string arrayStr = "";
+string[] mainArray = GetArrayString(arrayStr, count);
+Write($"[\"{String.Join("\", \"", mainArray)}\"] -> ");
+string[] selectedElements = SelectShortStrings(mainArray);
+Write($"[\"{String.Join("\", \"", selectedElements)}\"]");
+
+
+string[] GetArrayString(string ArrStr, int NumberOfWords)
+{
+    string[] result = new string[NumberOfWords];
+    for (int i = 0; i < result.Length; i++)
+    {
+        result[i] = ReadLine();
+    }
+    return result;
+}
+
+int CountNumber(string[] ArrStr)
+{
+    int count = 0;
+    for (int i = 0; i < ArrStr.Length; i++)
+    {
+        if (ArrStr[i].Length < 4)
+        {
+            count += 1;
+        }
+    }
+    return count;
+}
+
+string[] SelectShortStrings(string[] ArrStr)
+{
+    int j = 0;
+    string[] result = new string[CountNumber(ArrStr)];
+    for (int i = 0; i < ArrStr.Length; i++)
+    {
+        if (ArrStr[i].Length < 4)
+        {
+            result[j] = ArrStr[i];
+            j++;
+        }
+    }
+    return result;
+}
+
